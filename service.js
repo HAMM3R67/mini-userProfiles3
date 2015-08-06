e064431d19caec57937976bb4f9b37df1c3acb90
HAMM3R67 <tcschauer@gmail.com>
2015-08-05 19:37:44 -0600

    went over some alternate methods to make promises

--- service.js
+++ service.js
@@ -12,6 +12,24 @@ app.service('mainService', function($http, $q) {
       deferred.resolve(parsedResponse)
     })
     return deferred.promise;
+    
+  // Rather than creating your own promise you can just piggyBack off of the API with the below code,
+  // this method allows you to not have to worry about handling errors that occur when
+    
+  //  $http.get('http://reqr.es/api/users?page=1')
+  //    .then(function(response){
+  //      return response.data.data
+  //  })
+  
+  //return $q(function(resolve, reject){
+  //  $.ajax({
+  //    method: 'GET'
+  //    url: 'http://reqr.es/api/users?page=1'
+      //})
+  //})
+    
   }
 
 });
+
+  
